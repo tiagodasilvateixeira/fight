1cdb903f8552ff8a54edf559f9da3abd45de4f1e
Tiago Teixeira <tiago.teixeira@esx.com.br>
2020-10-19 15:00:20 -0300

    Subindo os sprites e movimentos básicos do primeiro personagem

---
+++ Assets/Scripts/RyuController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RyuController : MonoBehaviour
+{
+    #region public proprierts
+        public int Speed = 5;
+        public float JumpForce = 500f;
+        public float GroundDistance = 2.2f;
+        public LayerMask GroundLayer;
+    #endregion
+
+    #region internal components and proprierts
+        new Rigidbody2D rigidbody2D;
+        Animator animator;
+    #endregion
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (Mathf.Approximately(input.x, 0.0f))
+        {
+            animator.SetBool("idle", true);
+        }
+        else
+        {
+            animator.SetBool("idle", false);
+            transform.position = rigidbody2D.position + (input * Speed * Time.deltaTime);
+        }
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, GroundDistance, GroundLayer))
+        {
+            animator.SetBool("grounded", true);
+        }
+        else
+        {
+            animator.SetBool("grounded", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("jump");
+            rigidbody2D.AddForce(Vector3.up * JumpForce);
+        }
+    }
+}
