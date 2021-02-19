1dde954133f85812637ba60f090068bffbabfc1e
Tiago Teixeira <tiagodasilvateixeira@outlook.com>
2021-02-19 00:03:33 -0300

    Continuando a alteração da classe FightController

--- Assets/Scripts/Controllers/FightController.cs
+++ Assets/Scripts/Controllers/FightController.cs
@@ -6,46 +6,37 @@ using UnityEngine.UI;
 
 public class FightController: GameController
 {
-    #region proprierts
-        public bool GamePaused { get; set; }
-        public bool GoToBackToMenu { get; set; }
-        public GameObject MenuPanel;
-        public GameObject InitRoundPanel;
-        public GameObject RyuGameObject;
-        public GameObject BlankaGameObject;
-        public GameObject TogglePlayer1;
-        public GameObject TogglePlayer2;
-        public Image MaskPlayer1;
-        public Image MaskPlayer2;
-        public Text CounterText;
-        public Round[] Rounds;
-        PlayerController Player1;
-        PlayerController Player2;
-        private float SecondsToFinishRound;
-        public float InitialRoundSeconds = 90.0f;
-        public int RoundsCount = 3;
-        public int CurrentRound = 0;
-        private float SecondsToImproveRound = 5.0f;
-        private float FinishTimer = 3.0f;
-    #endregion
+    public bool GamePaused { get; set; }
+    public bool GoToBackToMenu { get; set; }
+    public GameObject MenuPanel;
+    public GameObject InitRoundPanel;
+    public GameObject RyuGameObject;
+    public GameObject BlankaGameObject;
+    public GameObject TogglePlayer1;
+    public GameObject TogglePlayer2;
+    public Image MaskPlayer1;
+    public Image MaskPlayer2;
+    public Text CounterText;
+    public Round[] Rounds;
+    PlayerController Player1;
+    PlayerController Player2;
+    private float SecondsToFinishRound;
+    public float InitialRoundSeconds = 90.0f;
+    private float SecondsToImproveRound = 5.0f;
+    private float FinishTimer = 3.0f;
+    public int RoundsCount = 2;
+    public int CurrentRound = 0;
 
-    #region states
-        protected GameState FightState;
-    #endregion
+    protected GameState FightState;
 
     private void Start() 
     {
-        FightState = new FightState(this);
         Rounds = new Round[RoundsCount];
 
-        SetState(FightState);
-        
+        InitState();
         BindPlayerControllerToPlayerObject();
-        SetInitialPlayersLife();
-        SetInitialPlayersMaskWidth();
-        InitializePlayersCharacters();
-        StartRound(CurrentRound);
-        ResumeGame();
+        SetPlayersToTheNextRound();
+        StartRound();
     }
 
     private void Update() 
@@ -53,24 +44,30 @@ public class FightController: GameController
         if (FightIsOpen())
         {
             UpdateTimer();
-            if (APlayerDefeated())
+            RoundTimeIsOver();
+            if (APlayerHasNoLife())
             {
-                FinishRound(GetPlayerWithMoreLife());
-                SetInitialPlayersLife();
-                SetInitialPlayersMaskWidth();
-                InitializePlayersCharacters();
+                SetRoundWinner(GetPlayerWithMoreLife());
+                SetPlayersToTheNextRound();
                 if (FightIsOpen())
                 {
-                    StartRound(CurrentRound++);
+                    CurrentRound += 1;
+                    StartRound();
                 }
             }
-            CheckRoundSeconds();
+            
             GameState.Update();
         }
         else
         {
             FinishFight();
         }
+    }
+
+    void InitState()
+    {
+        FightState = new FightState(this);
+        SetState(FightState);
     }
 
     void BindPlayerControllerToPlayerObject()
@@ -87,16 +84,49 @@ public class FightController: GameController
         }
     }
 
-    void SetInitialPlayersLife()
+    void SetPlayersToTheNextRound()
+    {
+        InitPlayersLife();
+        SetPlayersMasks();
+        InitPlayersMaskWidth();
+        SetPlayersInputs();
+        InitPlayersPositions();
+    }
+
+    void InitPlayersLife()
     {
         Player1.Life = 1f;
         Player2.Life = 1f;
     }
 
-    void SetInitialPlayersMaskWidth()
+    void SetPlayersMasks()
+    {
+        Player1.Mask = MaskPlayer1;
+        Player2.Mask = MaskPlayer2;
+    }
+    
+    void InitPlayersMaskWidth()
     {
         HealthBarController.instance.SetInitialMaskWidth(Player1.Mask);
         HealthBarController.instance.SetInitialMaskWidth(Player2.Mask);
+    }
+
+    void SetPlayersInputs()
+    {
+        Player1.IA = false;
+        Player2.IA = true;
+    }
+
+    void InitPlayersPositions()
+    {
+        Player1.transform.position = new Vector3(-6f, -2.5f, 0f);
+        Player2.transform.position = new Vector3(6f, -2.5f, 0f);
+    }
+
+    void StartRound()
+    {
+        SecondsToFinishRound = InitialRoundSeconds;
+        Rounds[CurrentRound] = new Round(CurrentRound+1);
     }
 
     bool FightIsOpen()
@@ -128,68 +158,84 @@ public class FightController: GameController
     {
         if (CurrentRound < RoundsCount)
             return false;
-        Debug.Log("Is last round");
         return true;
     }
 
     bool LastRoundIsOver()
     {
         if (string.IsNullOrWhiteSpace(Rounds[CurrentRound]?.Winner))
-        {
             return false;
+        return true;
+    }
+
+    public void UpdateTimer()
+    {
+        if (SecondsToFinishRound <= InitialRoundSeconds)
+        {
+            SecondsToFinishRound -= Time.deltaTime;
+            CounterText.text = SecondsToFinishRound.ToString("0");
+        }
+    }
+
+    bool APlayerHasNoLife()
+    {
+        if (Player1.Life <= 0 || Player2.Life <= 0)
+            return true;
+        return false;
+    }
+
+    private string GetPlayerWithMoreLife()
+    {
+        if (Player1.Life > Player2.Life)
+        {
+            return Player1.Name;
+        }
+        else if (Player2.Life > Player1.Life)
+        {
+            return Player2.Name;
         }
         else
         {
-            return true;
-        }
-    }
-    
-    void InitializePlayersCharacters()
-    {
-        Player1.Mask = MaskPlayer1;
-        Player1.IA = false;
-        Player1.transform.position = new Vector3(-6f, -2.5f, 0f);
-        Player2.Mask = MaskPlayer2;
-        Player2.IA = true;
-        Player2.transform.position = new Vector3(6f, -2.5f, 0f);
-    }
-
-    public bool RoundStarting()
-    {
-        if (SecondsToFinishRound + 3f > InitialRoundSeconds)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
+            return "Draw";
         }
     }
 
-    void DisablePlayersAfterRoundEnd()
-    {
-        Player1.IA = true;
-        Player2.IA = true;
-    }
-
-    void StartRound(int RoundNumber)
-    {
-        SecondsToFinishRound = InitialRoundSeconds;
-        Rounds[RoundNumber] = new Round(RoundNumber+1);
-    }
-
-    public void FinishRound(string character)
+    public void SetRoundWinner(string character)
     {
         Rounds[CurrentRound].Winner = character;
-        if (character == Player1.name)
+        MarkRoundWinner();
+    }
+
+    void MarkRoundWinner()
+    {
+        if (Rounds[CurrentRound].Winner == Player1.name)
         {
-            Toggle togglePlayer1 = TogglePlayer1.GetComponent<Toggle>();
-            togglePlayer1.isOn = true;
+            SetPlayerToggleOn(TogglePlayer1.GetComponent<Toggle>());
         }
-        else if (character == Player2.name)
+        else if (Rounds[CurrentRound].Winner == Player2.name)
         {
-            Toggle togglePlayer2 = TogglePlayer2.GetComponent<Toggle>();
-            togglePlayer2.isOn = true;
+            SetPlayerToggleOn(TogglePlayer2.GetComponent<Toggle>());
+        }
+    }
+
+    void SetPlayerToggleOn(Toggle playerToggle)
+    {
+        playerToggle.isOn = true;
+    }
+
+    public void RoundTimeIsOver()
+    {
+        if (SecondsToFinishRound <= 0.0f)
+        {
+            string playerWithMoreLife = GetPlayerWithMoreLife();
+            if (playerWithMoreLife != "Draw")
+            {
+                SetRoundWinner(playerWithMoreLife);
+            }
+            else
+            {
+                SecondsToFinishRound = SecondsToImproveRound;
+            }
         }
     }
 
@@ -213,7 +259,7 @@ public class FightController: GameController
             BackToMenu();
         }
     }
-
+    
     string GetWinner()
     {
         if (Rounds[0]?.Winner == Rounds[1]?.Winner)
@@ -230,53 +276,16 @@ public class FightController: GameController
         }
     }
 
-    public void UpdateTimer()
+    public bool RoundStarting()
     {
-        if (SecondsToFinishRound <= InitialRoundSeconds)
+        if (SecondsToFinishRound + 3f > InitialRoundSeconds)
         {
-            SecondsToFinishRound -= Time.deltaTime;
-            CounterText.text = SecondsToFinishRound.ToString("0");
-        }
-    }
-
-    private string GetPlayerWithMoreLife()
-    {
-        
-        if (Player1.Life > Player2.Life)
-        {
-            return Player1.Name;
-        }
-        else if (Player2.Life > Player1.Life)
-        {
-            return Player2.Name;
+            return true;
         }
         else
         {
-            return "Draw";
+            return false;
         }
-    }
-
-    public void CheckRoundSeconds()
-    {
-        if (SecondsToFinishRound <= 0.0f)
-        {
-            string playerWithMoreLife = GetPlayerWithMoreLife();
-            if (playerWithMoreLife != "Draw")
-            {
-                FinishRound(playerWithMoreLife);
-            }
-            else
-            {
-                SecondsToFinishRound = SecondsToImproveRound;
-            }
-        }
-    }
-
-    bool APlayerDefeated()
-    {
-        if (Player1.Life <= 0 || Player2.Life <= 0)
-            return true;
-        return false;
     }
 
     public void HandlerInitRoundPanel(bool enableRoundPanel)
