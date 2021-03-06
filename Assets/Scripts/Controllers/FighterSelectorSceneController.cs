f6bbdd348b139a698abb2ba7d94f91f11718bfe3
Tiago Teixeira <tiagodasilvateixeira@outlook.com>
2021-03-06 19:57:58 -0300

    Travando o botão de iniciar a luta sem ter escolhido um personagem antes

--- Assets/Scripts/Controllers/FighterSelectorSceneController.cs
+++ Assets/Scripts/Controllers/FighterSelectorSceneController.cs
@@ -2,13 +2,38 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FighterSelectorSceneController: MonoSingleton<FighterSelectorSceneController>
 {
     public string FighterSelected { get; set; }
+    private Button InitFight
+    {
+        get
+        {
+            return GameObject.Find("ButtonFight").GetComponent<Button>();
+        }
+        set{}
+    }
 
     public void OpenScene()
     {
         SceneManager.LoadScene("FighterSelectorScene", LoadSceneMode.Single);
+    }
+
+    private void Update()
+    {
+        EnableInitFightButtonIfAFighterIsSelected();
+    }
+
+    private void EnableInitFightButtonIfAFighterIsSelected()
+    {
+        if (FighterSelected != null)
+            InitFight.interactable = true;
+    }
+    
+    public void SetSelectedFighter(string fighterName)
+    {
+        FighterSelected = fighterName;
     }
 }
