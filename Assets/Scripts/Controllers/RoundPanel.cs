5a7e2c4e4817a808fd8d4a2edfb4929415d7ff4a
Tiago Teixeira <tiagodasilvateixeira@outlook.com>
2021-10-31 17:07:53 -0300

    Desativando o input dos lutadores nos primeiros segundos de cada round

--- Assets/Scripts/Controllers/RoundPanel.cs
+++ Assets/Scripts/Controllers/RoundPanel.cs
@@ -1,37 +1,56 @@
+using Game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RoundPanel : MonoBehaviour
+namespace Controllers
 {
-    [SerializeField]
-    private Text CounterText;
-
-    void Start()
+    public class RoundPanel : MonoBehaviour
     {
-        DisableRoundFightImage();
-    }
+        [SerializeField]
+        private Text CounterText;
 
-    private void Update()
-    {
-        if (int.Parse(CounterText.text) > 85)
-        {
-            EnableRoundFightImage();
-        }
-        else
+        void Start()
         {
             DisableRoundFightImage();
         }
-    }
 
-    public void EnableRoundFightImage()
-    {
-        gameObject.GetComponent<Image>().enabled = true;
-    }
+        private void Update()
+        {
+            StartCoroutine(TransformImageVisibility());
+        }
 
-    public void DisableRoundFightImage()
-    {
-        gameObject.GetComponent<Image>().enabled = false;
+        IEnumerator TransformImageVisibility()
+        {
+            if (int.Parse(CounterText.text) > 85)
+            {
+                EnableRoundFightImage();
+                ChangeCharactersInputStatus(false, false);
+            }
+            else
+            {
+                DisableRoundFightImage();
+                ChangeCharactersInputStatus(true, true);
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+
+        void EnableRoundFightImage()
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+        }
+
+        void DisableRoundFightImage()
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+        }
+
+        void ChangeCharactersInputStatus(bool characterOneEnable, bool characterTwoEnable)
+        {
+            CallFighters.Instance.CharacterPlayerOne.CharacterInput.Enabled = characterOneEnable;
+            CallFighters.Instance.CharacterPlayerTwo.CharacterInput.Enabled = characterTwoEnable;
+        }
     }
 }
